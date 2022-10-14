be7b8589a0b1860e9303e3d14041e30fb63db65f
Mahamudul-Hasan-shotej <shootej@gmail.com>
2022-10-14 15:52:54 +0600

    CardImageAdded & Whatsapp link corrected

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -5,17 +5,51 @@ import img from "../Assets/Images/quote_image.jpg";
 import CardElement from "../components/Dashboard/CardElement";
 import { CardText } from "../components/Dashboard/CardText";
 import Slider from "../components/Dashboard/Slider";
+import ImageCard from "../components/Dashboard/ImageCard";
+import OneCardImage from "../Assets/Images/OneCardImage.jpg";
+import TwoCardImage from "../Assets/Images/TwoCardImage.jpg";
+import ThreeCardImage from "../Assets/Images/ThreeCardImage.jpg";
 
 const Dashboard = () => {
+  var text =
+    "This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.";
+
   return (
     <div className="Dashboard">
       <div className="Header">
         <Header></Header>
       </div>
-      <div className ="Slider">
-      <Slider/>
+      <div className="Slider">
+        <Slider />
       </div>
-      
+
+      <div className="ImageCard-bg">
+        <div className="ImageCard">
+          <ImageCard
+            image={OneCardImage}
+            text="A"
+            contentText={text}
+            title="Drawing Room Decoration"
+            subHeader="Arch-tech Owened design"
+          ></ImageCard>
+          <ImageCard
+            image={TwoCardImage}
+            text="A"
+            contentText={text}
+            title="Family Living standard"
+            subHeader="Arch-tech Owened design"
+          ></ImageCard>
+          <ImageCard
+            image={ThreeCardImage}
+            text="B"
+            contentText={text}
+            title="Classic Living room"
+            subHeader="Arch-tech Owened design"
+          ></ImageCard>
+          {/* <ImageCard image={ThreeCardImage}></ImageCard> */}
+        </div>
+      </div>
+
       <div className="Quote">
         <div className="cardText">
           <CardText></CardText>
@@ -24,9 +58,7 @@ const Dashboard = () => {
           <CardElement></CardElement>
         </div>
       </div>
-      
     </div>
-
   );
 };
 
--- src/components/Dashboard/Whatsapp.js
+++ src/components/Dashboard/Whatsapp.js
@@ -1,31 +1,37 @@
-import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 import "bootstrap/dist/css/bootstrap.css";
-import React, { useState } from 'react';
-import Modal from 'react-bootstrap/Modal';
+import React, { useState } from "react";
+import Modal from "react-bootstrap/Modal";
 
-import './contact.css';
+import "./contact.css";
 
 const Whatsapp = () => {
+  const [value, setValue] = useState();
 
-    const [value, setValue] = useState(); 
-  
-    const [show, setShow] = useState(false);
-  
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    
-    
-      
-    return (
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => {
+    window.open("https://www.whatsapp.com/");
+  };
+
+  return (
     <>
-    
-        <div className='btn-click1' variant="dark" onClick={handleShow} data-open='Call Us'  
-         data-animation-in='fade-in' data-animation-out='fade-out'
-          aria-controls='Call Us' aria-haspopup='true' tabIndex='0' >
-         <WhatsAppIcon className='Whatsapp-icon'/>
-        </div>
-  
-        <Modal show={show} onHide={handleClose} className='formbody'>
+      <div
+        className="btn-click1"
+        variant="dark"
+        onClick={handleShow}
+        data-open="Call Us"
+        data-animation-in="fade-in"
+        data-animation-out="fade-out"
+        aria-controls="Call Us"
+        aria-haspopup="true"
+        tabIndex="0"
+      >
+        <WhatsAppIcon className="Whatsapp-icon" />
+      </div>
+
+      {/* <Modal show={show} onHide={handleClose} className='formbody'>
                 <Modal.Header closeButton>
                   <Modal.Title>Feel free to call : 01735698841</Modal.Title>
                   
@@ -34,18 +40,9 @@ const Whatsapp = () => {
                 <Modal.Footer>
                 <button className='btn-slider' type='submit' onClick={handleClose}>Call</button>
                 </Modal.Footer>
-              </Modal>
-        
-       
-        </>
-    );
-    
-  };
-  
- export default Whatsapp
-      
-     
-    
-  
-  
+              </Modal> */}
+    </>
+  );
+};
 
+export default Whatsapp;
